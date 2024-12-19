0718614b3d4693ea726beb87b873740f6c126a7b
Hassan Zahid Khan <hzk3903159@gmail.com>
2024-12-19 10:38:13 -0800

    Todo_app

--- core/core/urls.py
+++ core/core/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from portfolio.urls import *
-
+from todo.urls import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('portfolio.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
+    path('',include('todo.urls')),
 
+
+    path("__reload__/", include("django_browser_reload.urls")),
+    
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
